3474891a34f188241d1738b3f6b64a185c563785
rab8823 <no>
2021-12-02 06:57:11 -0500

    Solve day 1

---
+++ Problem.cs
@@ -0,0 +1,7 @@
+namespace AdventOfCode2021
+{
+    public abstract class Problem
+    {
+        public abstract void Solve();
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace AdventOfCode2021
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            new Day1().Solve();
+        }
+    }
+}
